b2e3f189d662c38315f1f53f21e24330a63539cc
kurosakananako <kurosakananako@localhost>
2020-05-18 08:25:43 +0900

    演習1−1の追加(好きな食べ物)

---
+++ soft_kadai1/src/soft_kadai1/kadai1_1.java
@@ -0,0 +1,14 @@
+package soft_kadai1;
+
+public class kadai1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("好きな食べ物: "+"お味噌汁(全般)");
+	
+	System.out.println("理由: "+"揚げなすの入った白味噌のお味噌汁の場合だと、味噌の甘さがナスのジューシーさをより際立たせ、とても旨味があるから。");
+	
+	}
+
+}
